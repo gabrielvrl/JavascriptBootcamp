6893664407db8015bd808bb15084303bdbdc24e5
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-07-19 21:26:33 -0300

    If Statements

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("It's Working!")
+if (1 !== 1) {
+  console.log("It's True!")
+}
+
+// Example 2
+
+let rating = 3;
+
+if(rating === 3){
+  console.log('YOU ARE A SUPERSTAR!')
+}
+
+// Example 3
+let num = 37;
+
+if(num % 2 !== 0){
+  console.log('ODD Number!')
+}
