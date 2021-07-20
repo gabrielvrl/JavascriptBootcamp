2091e10a2aeda34be826629d207fc9f00c7de8af
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-07-19 21:36:33 -0300

    Nesting Conditionals

--- app.js
+++ app.js
@@ -34,3 +34,18 @@ if(userScore >= highScore){
 else {
   console.log(`Good Game. Your score of ${userScore} did not beat the high score of ${highScore}`)
 }
+
+// Example 5
+let password = 'kitty';
+
+if(password.length >= 6){
+  if(password.indexOf(' ') === -1){
+    console.log('Valid Password!')
+  }
+  else {
+    console.log('Password is long enough, but cannot contain spaces')
+  }
+}
+else {
+  console.log('Password must be longer!')
+}
